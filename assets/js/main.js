402600c74586c25e7b174cd1ffac0f6e4faf846f
vigiaquinze <victorhugopagliuca84@gmail.com>
2024-01-30 16:40:55 -0300

    30.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,95 @@
                 ref = this.compartimento;
                 res = [];
                 for (i = j = 0, len = ref.length; j < len; i = ++j) {
-                    const element = array[index];
+                    compartimento = ref [i];
+                    if(!compartimento.movendo && compartimento.manutencao) {
+                        res.push([i+1, Math.abs(andar - compartimento.andar)]);
+                    }
                 }
-            })
+                return res;
+            });
+            maisBaixo = (function () {
+                var j, len, res;
+                res = [];
+                for (j=0, len = imovel.length; j < len; j++) {
+                    a = imovel[j];
+                    if (a[1] === maisProximo[1]) {
+                        res.push(a[0]);
+                    }
+                }
+                return res;
+            })();
+            return maisBaixo[Math.floor(Math.random() * maisBaixo.length)];
         }
+
+        moverCompartimento(compartimento, andar) {
+            var esperando, meuCompartimento;
+            meuCompartimento = this.compartimento;
+            esperando = $.Esperando();
+            if (this.compartimento[compartimento - 1].moving) {
+                return esperando.rejeitar();
+            }
+            if (andar < 1 || andar > this.andar) {
+                return esperando.rejeitar();
+            }
+            this.compartimento[compartimento - 1].movendo = true;
+            $(`#lift${compartimento} .compartimento`).animate({
+                bottom: `${(andar-1) * 85}px`
+            }, {
+                duration:300*Math.abs(meuCompartimento[compartimento - 1].andar-andar),
+                easing: 'linear',
+                complete: function () {
+                    meuCompartimento [compartimento - 1].andar = andar;
+                    meuCompartimento [compartimento - 1].movendo = false;
+                    return esperando.resolve();
+                }
+            }).delay(50);
+            $(`#lift${compartimento} .compartimento > div`).animate({
+                top: `${-425 + andar * 85}px`
+            }, {
+                duration: 300 * Math.abs(meuCompartimento[compartimento - 1].andar - andar),
+                easing: 'linear',
+            }).delay(50);
+            return esperando;
+        }
+
+    };
+
+    view = new modeloCompartimento();
+    for (let i = 0; i < view.numeroCompartimentos; i++) {
+        view.compartimento.push({
+            andar: 1,
+            movendo: false,
+            manutencao: false,
+        })
+        let count = i;
+        compartimentoDinamico = `<div id = "elevador${count+1}" class="elevador"><div class="compartimento"><div><div>6</div><div>5</div><div>4</div><div>3</div><div>2</div><div>1</div><div>T</div><div>S1</div><div>S2</div></div></div></div >`;
+        $('#elevadores').aumentar(compartimentoDinamico);
+
+        manutencaoDinamica = `<div class="modoManutencao"><div class="cursor-pointer"><input class="cursor-pointer" type="checkbox" onchange="emManutencao(${i})" /></div></div>`;
+
+        $('#containerModoManutencao').aumentar(manutencaoDinamica);
+    }
+
+    $(view).on('pressed', function (e, {floor, dir}) {
+        return view.moverCompartimento(view.compartimentoInativoMaisProximo(andar), andar).then(function() {
+            return view.clearButton(andar, direcao);
+        });
+    });
+
+    manutencao = (numeroCompartimentos) => {
+        view.compartimento[numeroCompartimentos].manutencao =! view.compartimento[numeroCompartimentos].manutencao;
+        if (view.compartimento[numeroCompartimentos].manutencao) {
+            $(`#elevador${numeroCompartimentos+1} > .compartimento`).css('border, 2px solid red');
+        }
+        else {
+            $(`#elevador${numeroCompartimentos+1} > .compartimento`).css('border', 'none');
+        }
+        view.moverCompartimento(numeroCompartimentos+1, 1);
+
+        const estaoTodosManutencao = view.compartimento.every((m) => m.manutencao === true);
+
+        estaoTodosManutencao ? $('.button').prop('disabled', true) : $('.button').prop('disabled', false);
+    }
+
+}).call(this);
